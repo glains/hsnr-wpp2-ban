56894e1f17125588735d354d1bf5658ea103a8a7
Colin Gebler <colin.gebler@gebler-online.de>
2021-06-02 15:56:29 +0200

    feat: initial training

--- unet/train_unet.py
+++ unet/train_unet.py
@@ -1,10 +1,15 @@
+from tensorflow.python.keras.callbacks import EarlyStopping
 from vendor import unet
 import tensorflow as tf
 from pathlib import Path
 from matplotlib import pyplot as plt
 import math
+import argparse
+from vendor.unet import unet
 
-def load_data(xPath:Path, yPath:Path):
+
+
+def load_data(xPath:Path, yPath:Path, batch_size:int):
     x_files = tf.data.Dataset.list_files(str(xPath.joinpath(Path("*.png"))), shuffle=False)
     y_files = tf.data.Dataset.list_files(str(yPath.joinpath(Path("*.png"))), shuffle=False)
 
@@ -19,37 +24,63 @@ def load_data(xPath:Path, yPath:Path):
         x_enc = tf.io.read_file(thing[0])
         x_dec = tf.image.decode_png(x_enc, channels=1)
         
-        x_vals.append(x_dec)
+        x_vals.append(tf.cast(x_dec, tf.float32))
 
         y_enc = tf.io.read_file(thing[1])
         y_dec = tf.image.decode_png(y_enc, channels=1)
 
-        y_vals.append(y_dec)
+        y_vals.append(tf.cast(y_dec, tf.float32))
 
-        imgCount = imgCount +1 
+        imgCount += 1
     
     x_data = tf.data.Dataset.from_tensor_slices(x_vals)
     y_data = tf.data.Dataset.from_tensor_slices(y_vals)
-
+    
     full_data = tf.data.Dataset.zip((x_data, y_data))
 
     #Shuffling & Batching
     full_data = full_data.shuffle(3,reshuffle_each_iteration=False)
-    full_data = full_data.batch(5)
-    trainData = full_data.take(math.floor(imgCount*0.8))
-    testData = full_data.skip(math.floor(imgCount*0.8)).take(math.floor(imgCount*0.1))
-    validationData = full_data.skip(math.floor(imgCount*0.9)).take(math.floor(imgCount*0.1))
+    full_data = full_data.batch(batch_size, drop_remainder=True)
+
+    batchCount = math.floor(imgCount/batch_size)
+    trainData = full_data.take(math.floor(batchCount*0.8))
+    testData = full_data.skip(math.floor(batchCount*0.8)).take(math.floor(batchCount*0.1))
+    validationData = full_data.skip(math.floor(batchCount*0.9)).take(math.floor(batchCount*0.1))
 
     return trainData, testData, validationData
+
+def build_unet(batch_size: int, filters_orig: int) -> tf.keras.Model:
+    # Input Layer
+    x = tf.keras.Input(shape=(256, 256, 1), batch_size=batch_size)
+    # Hidden Layer
+    tensor = unet(x, filters_orig=filters_orig, out_channels=7, final_activation='linear', batch_norm=False)
+
+    model = tf.keras.Model(inputs=x,outputs=tensor)
+    model.compile(
+        optimizer="Adam",
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy']
+    )
+
+    return model
+
 if __name__=="__main__":
-    train, test, val = load_data(Path("C:/Users/Colin/Documents/WPP/out/images/B_BL_TSE_DIXON_CONTROL_LEG_45MIN_OPP_0041"), Path("C:\\Users\\Colin\\Documents\\WPP\\out\\labels\\B_BL_TSE_DIXON_CONTROL_LEG_45MIN_OPP_0041"))
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-i', type=Path, action='store', dest='inputPath', required=True)
+    parser.add_argument('-bs', type=int, action='store', dest='batchSize', default=5)
+    parser.add_argument('-flt', type=int, action='store', dest='filtersOrig', default=16)
+
+    args = parser.parse_args()
+
+    train, test, val = load_data(args.inputPath.joinpath(Path('images')), args.inputPath.joinpath(Path('labels')), args.batchSize)
 
     for e in train.take(1):
         print('image shape=%s' % e[0].get_shape().as_list())
         print('label shape=%s' % e[1].get_shape().as_list())
+ 
+    model = build_unet(args.batchSize, args.filtersOrig)
     
-
-
-    print("Hello world!")
+    model.fit(train, epochs=100, validation_data=val, callbacks=[EarlyStopping(monitor='val_loss', patience=3, restore_best_weights = True)])
 
 
